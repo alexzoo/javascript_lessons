8293fb9b9a70d10bf7556410e042944e7ebf5ae0
aleksey.zubov <aleksey.zubov@rt.ru>
2020-03-22 14:26:33 +0300

    вариант с подстрокой

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -3,14 +3,17 @@
 // Создайте массив week и запишите в него дни недели в виде строк (вручную).
 // Выведите на экран все дни недели, каждый из них с новой строчки, а выходные дни - жирным шрифтом. Текущий день - курсивом (пока можно задать текущий день вручную, без работы с объектом даты)
 
-let weekdays = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let weekdays = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+   output = document.querySelector('.output');
+
 
 function printWeekdays() {
-   for (let i = 0; i < 5; i++) {
-      document.write(weekdays[i] + '<br>');
+   for (let i = 0; i < weekdays.length - 1; i++) {
+      if (weekdays.indexOf('суббота') || weekdays.indexOf('воскресенье')) {
+         output.innerHTML += '<b>' + weekdays[i] + '</b>' + '<br>';
+      }
+      output.innerHTML += weekdays[i] + '<br>';
    }
-   document.write('<b>' + weekdays[5] + '</b>' + '<br>');
-   document.write('<i>' + weekdays[6] + '</i>' + '<br>');
 }
 
 printWeekdays();
