eb9059db3e000adf9d5f895f2b922c90bf9a02d5
aleksey.zubov <aleksey.zubov@rt.ru>
2020-03-24 22:05:58 +0300

    сделал задание

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -31,11 +31,40 @@ let arr = [20, 33, 1, 'Человек', 2, 3];
 let res3 = arr.filter(Number);
 let sum = 0;
 
-for (let i = 0; i < res3.length - 1; i++) {
-   let res = (res3[i] ** 3);
-   sum += res[i];
-   console.log(sum);
+// Второй способ с forEach
+
+res3.forEach(num => {
+   sum += num ** 3;
+});
+console.log(sum);
+
+//
+res3.forEach(num => sum += num ** 3); // Максимально короткий
+console.log(sum);
+
+
+// Создайте функцию, которая принимает 1 аргумент. (название произвольное) Если передана не строка - функция оповещает об этом пользователя. 
+// В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце. Если строка более 50 знаков - то после 50го символа часть текста скрывается и вместо них появляются три точки (...)
+
+function manipulateString(string) {
+   if (typeof (string) != 'string') {
+      console.log('Не строка');
+      return false;
+   }
+
+   let res = string.replace(/(^\s+|\s+$)/g, '');
+   console.log('>' + res + '<');
+
+   if (string.length > 50) {
+      let res = string.slice(0, 51) + '...';
+      console.log(res);
+   }
+
 }
+
+manipulateString(123);
+manipulateString('   privet    ');
+manipulateString('Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod eum sapiente delectus aut corporis inventore laudantium provident earum consequuntur. Amet, quod? Repellendus excepturi dolor ducimus harum alias sunt ad tempore.');
 
 
 
