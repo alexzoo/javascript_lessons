fe11237dc2e2197e4624f8cb6fb211bc374cade0
aleksey.zubov <aleksey.zubov@rt.ru>
2020-04-04 18:37:11 +0300

    вариант через bind

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -52,4 +52,7 @@ function showUser(surname, name) {
 	alert(`Пользователь ${surname} ${name}, его возраст ${this.value}`);
 }
 
-showUser.apply(age, ['Пупкин', 'Вася']);
+// showUser.apply(age, ['Пупкин', 'Вася']);
+
+let newFunc = showUser.bind(age, 'Пупкин', 'Вася');
+newFunc();
