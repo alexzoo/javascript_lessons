32c84ff8c18b0acb860126a209cc7f852996983f
aleksey.zubov <aleksey.zubov@rt.ru>
2020-04-05 10:44:25 +0300

    задача с классом

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -46,13 +46,46 @@
 
 // Выведите на экран правильное сообщение, которое берет значение из input
 
-let age = document.getElementById('age');
+// let age = document.getElementById('age');
 
-function showUser(surname, name) {
-	alert(`Пользователь ${surname} ${name}, его возраст ${this.value}`);
+// function showUser(surname, name) {
+// 	alert(`Пользователь ${surname} ${name}, его возраст ${this.value}`);
+// }
+
+// // showUser.apply(age, ['Пупкин', 'Вася']);
+
+// let boundShowUser = showUser.bind(age, 'Пупкин', 'Вася');
+// boundShowUser();
+
+// 2) Используя синтаксис ES6 в отдельном документе:
+
+// ·        Создать класс options
+
+// ·        Он должен содержать свойства: height, width, bg, fontSize, textAlign
+
+// ·        Он должен содержать метод, создающий новый div на странице, записывающий в него любой текст и при помощи cssText изменять свой стиль из переданных параметров
+
+// ·        Создать новый объект через класс
+
+// ·        Вызвать его метод и получить элемент на странице
+
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	createDiv(text, style) {
+		let div = document.createElement('div');
+		div.style.cssText = style;
+		div.innerHTML = text;
+		return div;
+	}
 }
 
-// showUser.apply(age, ['Пупкин', 'Вася']);
-
-let boundShowUser = showUser.bind(age, 'Пупкин', 'Вася');
-boundShowUser();
+let newOptions = new Options('500', '200', 'red', '15', 'center');
+let newDiv = newOptions.createDiv('test', 'color: green');
+console.log(newDiv);
