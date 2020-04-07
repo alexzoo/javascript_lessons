8525e7fe07b853ebe4aa68d8e7ba7ba26ca1cea0
aleksey.zubov <aleksey.zubov@rt.ru>
2020-04-07 21:35:43 +0300

    eventLast

--- checkio/script.js
+++ checkio/script.js
@@ -35,3 +35,36 @@ function secondIndex(text, symbol) {
 }
 
 console.log(secondIndex('sims', 's'));
+
+//сделать букву предложения большой, и добавить в конец точку если ее нет.
+
+function correctSentence(text) {
+	text = text.charAt(0).toUpperCase() + text.substr(1);
+	if (text.charAt(text.length - 1) != '.') {
+		text += text.charAt(text.length) + '.';
+	}
+	return text;
+}
+
+console.log(correctSentence('greetings, friends.'));
+
+// Дан массив целых чисел. Нужно найти сумму элементов с четными индексами (0-й, 2-й, 4-й итд), затем перемножить эту сумму и последний элемент исходного массива. Не забудьте, что первый элемент массива имеет индекс 0.
+
+// Для пустого массива результат всегда 0 (ноль).
+
+function evenLast(data) {
+	let sum = 0;
+
+	if (data.length == 0) {
+		return 0;
+	}
+
+	for (let i = 0; i < data.length; i += 2) {
+		sum += data[i];
+	}
+	sum *= data[data.length - 1];
+
+	return sum;
+}
+
+console.log(evenLast([]));
