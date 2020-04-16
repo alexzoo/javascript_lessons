c5cb339e952abd265e9374070f05c029c2ae5761
aleksey.zubov <aleksey.zubov@rt.ru>
2020-04-16 03:53:09 +0300

    сделал currency на промисах

--- currency/js/script.js
+++ currency/js/script.js
@@ -1,22 +1,27 @@
+'use strict';
 
 let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+	inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+	// let request = new XMLHttpRequest();
 
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+	// request.open('GET', 'js/current.json');
+	// request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	// request.send();
 
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-        }
-    });
+	fetch('js/current.json')
+		.then((data) => data.json())
+		.then((data) => (inputUsd.value = inputRub.value / data.usd))
+		.catch((inputUsd.value = 'Что-то пошло не так!'));
 
+	// request.addEventListener('readystatechange', function () {
+	// 	if (request.readyState === 4 && request.status == 200) {
+	// 		let data = JSON.parse(request.response);
+
+	// 		inputUsd.value = inputRub.value / data.usd;
+	// 	} else {
+	// 		inputUsd.value = 'Что-то пошло не так!';
+	// 	}
+	// });
 });
