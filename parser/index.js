cd3986b3a51eefa2fef915623e59c462ed918ead
aleksey.zubov <aleksey.zubov@rt.ru>
2020-05-19 21:48:35 +0300

    добавил функцию создания карточки

--- parser/index.js
+++ parser/index.js
@@ -10,14 +10,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			let doc = parser.parseFromString(html, 'text/html');
 
-			let items = doc.querySelectorAll('.gbGoodsItem');
+			const goods = [];
 
-			items.forEach((elem) => {
-				console.log(elem);
-				// document.body.appendChild(elem);
+			doc.querySelectorAll('.gbGoodsItem').forEach((elem) => {
+				const item = {};
+				item.title = elem.querySelector('.gbGoodsItem_title').getAttribute('title');
+				item.img = elem.querySelector('.gbGoodsItem_image').getAttribute('data-lazy');
+				item.price = elem.querySelector('.gbGoodsItem_price').textContent;
+				goods.push(item);
 			});
+			console.log(goods);
+
+			createCard(goods);
 		})
+		// .then((data) => createCard(data))
 		.catch(function (err) {
-			console.log('Failed to fetch page: ', err);
+			console.log('Произошла ошибка при загрузке: ', err);
 		});
+
+	function createCard(data) {
+		data.forEach(({ title, img, price }) => {
+			const element = document.createElement('div');
+
+			element.classList.add('item');
+
+			element.innerHTML = `
+				<img src=${img}>
+				<h3 class="item__title">${title}</h3>
+				<div class="item__price">
+					${price}$
+				</div>
+			`;
+			document.querySelector('.container').append(element);
+		});
+	}
 });
